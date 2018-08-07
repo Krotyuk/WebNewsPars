4efb635716b1c7f36e69690231f83259f84d03a3
Krotyuk <kosonshow@gmail.com>
2018-08-07 19:28:21 +0700

    Update WebNewsPars.py

--- WebNewsPars.py
+++ WebNewsPars.py
@@ -1,50 +1,57 @@
 import urllib.request
+
 import re
 import sys
+import chardet
 
-class ParseMyClass:
+
+class tensorParseTest:
     def __init__(self, url):
         self.url = url
         self.file = '-'.join(self.url.split('/'))[8:]
         site = urllib.request.urlopen(self.url)
-        html = str(site.read().decode('utf-8'))
+        kodirovka = chardet.detect(urllib.request.urlopen(url).read())['encoding']
+        html = str(site.read().decode(str(kodirovka)))
+
         self.article = re.findall(r'<article[^>]*>(.*?)</article>', str(html), re.DOTALL)
         if len(self.article) == 0:
             self.article = re.findall(r'<div.*itemtype="http://schema.org/NewsArticle[^>]+?>(.*)</div>', str(html),
                                       re.DOTALL)
-        self.razbor()
-        self.save()
+        if 'gazeta' in url:
+            self.article = re.findall(r'<p[^>]*>(.*?)</p>', str(html), re.DOTALL)
+        self.reClean()
+        self.saveFileTxt()
 
-    def razbor(self):
+    # функция очистки контента
+    def reClean(self):
         article = self.article
 
-        # меняем ссылки
+        # меняем ссылки на формат "[]"
         article = re.sub(r'<[aA]\s{1}href=[\'\"](.*?)[\'\"][^>]*>(.*?)</[aA]>', r'[URL="\1"] \2 [/URL]', str(article))
 
-        #		#Тут надо будет подумать, как получше это сделать
-        #		#выбираем фотки
-        #		article = re.sub(r'<img(.*?)alt=[\'\"](.*?)[\'\"](.*?)src=[\'\"](.*?)[\'\"](.*?)>', r'[IMG="\4"]\2[/IMG]', str(article))
+        # выделяем фотографии в ссылки
+        article = re.sub(r'<img(.*?)alt=[\'\"](.*?)[\'\"](.*?)src=[\'\"](.*?)[\'\"](.*?)>', r'[IMG="\4"]\2[/IMG]', str(article))
 
-        # удаляем различные теги для чистоты
+        # убираем теги
         article = re.sub(r'<(section|script|aside|time).*?</\1>(?s)', '', str(article))
         article = re.sub(r'<div class="bordered-title">(.*)</div>', '', str(article))
 
-        # удаляем лишние пробелы
+        # убираем лишние пробелы
         article = re.sub(r'\s+', ' ', str(article))
 
-        # удаляем табуляцию
+        # убираем табуляцию
         article = re.sub(r'\t', r'\s', str(article))
 
         # выделяем заголовки
         article = re.sub(r'<[hH][^>]*>(.*?)</[hH][^>]*>', r'\1\r\n', str(article))
 
         # выделяем абзацы
-        article = re.sub(r'<[pP][^>]*>(.*?)</[pP][^>]*>', r'\1\r\n', str(article))
+        article = re.sub(r'<[pP][^>]*>(.*?)</[pP][^>]*>', r'\1\r\n\n', str(article))
 
         # удаляем все оставльные теги
         article = re.sub(r'<.*?>', '', str(article))
 
-        # Выравниваем по 80 символов
+        # Выравниваем тест по формату "не больше 80 символов"
         article.strip()
         list = article.split('\r\n')
         i = 0
@@ -54,7 +61,7 @@ class ParseMyClass:
                 shag = 1
                 k = j * shag
                 nachalo_stroki = 0
-                while 0 < k <= len(list[i]):
+                while 0 < k <= len(list[i])-1:
                     if ord(list[i][k]) != 32:
                         while list[i][k] is not None and list[i][k] != ' ' and list[i][
                             k] != '\n' and k > nachalo_stroki:
@@ -69,9 +76,10 @@ class ParseMyClass:
                     nachalo_stroki = k - j
             i = i + 1
         article = str('\r\n'.join(list))
-        self.article = article
 
-    def save(self):
+        self.article = article
+    #сохранение в файл
+    def saveFileTxt(self):
         fname = self.file + '.txt'
         print(fname)
         f = open(fname, 'w')
@@ -80,8 +88,8 @@ class ParseMyClass:
 
 
 if len(sys.argv) > 1:
-    parse = ParseMyClass(sys.argv[1])
+    parse = tensorParseTest(sys.argv[1])
 else:
     url = input('Введите URL статьи ')
     if url is not None and len(url.strip()) > 0:
-        parse = ParseMyClass(url)
+        parse = tensorParseTest(url)
